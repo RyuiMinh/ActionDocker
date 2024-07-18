831ce70125aee8479341d7727d0e3878d32e5063
RyuiMinh <167089278+RyuiMinh@users.noreply.github.com>
2024-07-18 15:27:27 +0700

    Update setup.sh

--- bacom/two/setup.sh
+++ bacom/two/setup.sh
@@ -5,11 +5,9 @@ tar -zxvf ngrok.tgz
 ./ngrok config add-authtoken 2caMIyH98jojFijfUDA9HHKZDZ0_2naziucGo1SjGJ4xWL8QM
 ./ngrok http 8006 --region ap &>/dev/null &
 sudo mkdir /isos/
-sudo wget -O /isos/a.iso https://computernewb.com/isos/windows/Windows%20Server%202012%20R2.iso &>/dev/null &
-while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
+sudo wget -O /isos/a.iso https://computernewb.com/isos/windows/Win10_22H2_English_x64.iso
 sudo wget -O /isos/driver.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/
-virtio-win-0.1.240-1/virtio-win-0.1.240.iso &>/dev/null &
-while ps axg | grep -vw grep | grep -w wget > /dev/null; do sleep 1; done
-wget https://raw.githubusercontent.com/RyuiMinh/ActionDocker/main/compose.yaml
+virtio-win-0.1.240-1/virtio-win-0.1.240.iso
+wget https://raw.githubusercontent.com/RyuiMinh/ActionDocker/main/bacom/two/compose.yaml
 echo "Successfully connected to port 8006"
 sudo docker compose up
